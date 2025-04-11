72ade46a733357b92c4bee35dbf092e0a625d6a3
Damian Bruque <dbruque@galiciaseguros.com.ar>
2025-04-11 13:14:47 -0300

    Configuracion de DbContext

---
+++ Dominio/Entidades/Estado_His.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dominio.Entidades;
+
+public class Estado_His
+{
+
+    public int Id { get; set; } // Primary Key
+    public int EstadoId { get; set; } // Foreign Key to Estados
+    [ForeignKey("EstadoId")]
+    public Estado Estado { get; set; } // Navigation property to Estados
+    [Required]
+    public string Nombre { get; set; } // Nombre del estado
+    [Required]
+    public string Descripcion { get; set; } // Descripción del estado
+    [Required]
+    public DateTime FechaCreacion { get; set; } // Fecha de creación del registro
+    [Required]
+    public DateTime FechaModificacion { get; set; } // Fecha de modificación del registro
+    [Required]
+    public string Action { get; set; } // Acción realizada (creado, modificado, eliminado)
+}
